31eae20ccee7cd4e3c5316aefcc18f15b15cd859
MJonas <mateusjonass23@gmail.com>
2021-10-04 19:01:31 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+# primeiro importar tkinter
+from tkinter import *
+from tkinter import ttk
+
+#fazendo o tamnaho da janela
+janela = Tk()
+janela.title("calculadora")
+janela.geometry("260x350")
+
+janela.mainloop()
